82777b69ddbf3af4c99c6dc28a1d9b9a491abd74
harshkesarwani1501 <kesarwaniharsh15@gmail.com>
2026-09-07 15:52:50 +0000

    added workflows

--- app/app.py
+++ app/app.py
@@ -21,7 +21,7 @@ r = redis.Redis(
 def index():
     count = r.incr("hits")
     return jsonify({
-        "message": "Hello from Fluid AI DevOps challenge",
+        "message": "Hello from Fluid AI DevOps challenge1",
         "hits": count,
         "pod": os.environ.get("HOSTNAME", "unknown"),
     })
